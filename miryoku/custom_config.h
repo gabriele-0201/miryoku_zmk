b0030476f43849f9c73fa4ecd777bd593ae55d96
gabriele-0201 <gabriele.miotti2@gmail.com>
2024-06-15 21:41:38 +0200

    ferris worklfow

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,3 +1,52 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
+// define MIRYOKU_ALPHAS_QWERTY
+// #define MIRYOKU_TAP_QWERTY
+// #define MIRYOKU_NAV_VI
+// #define MIRYOKU_KLUDGE_MOUSEKEYSPR
+
+// LAYER from qmk version
+// #define MIRYOKU_LAYER_BASE \
+// KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, \
+// LGUI_T(KC_A), LALT_T(KC_S), LCTL_T(KC_D), LSFT_T(KC_F), KC_G, KC_H,
+// LSFT_T(KC_J), LCTL_T(KC_K), LALT_T(KC_L), LGUI_T(KC_QUOT), \
+// LT(U_BUTTON, KC_Z), ALGR_T(KC_X), KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM,
+// ALGR_T(KC_DOT), LT(U_BUTTON, \ KC_SLSH), U_NP, U_NP, LT(U_MEDIA, KC_TAB),
+// LT(U_NAV, KC_ESC), LT(U_MOUSE, KC_BSPC), LT(U_SYM, KC_SPC), LT(U_NUM,
+// KC_ENT), LT(U_FUN, KC_DEL), U_NP, U_NP
+
+////#define MIRYOKU_LAYER_BASE \
+////&kp Q,             &kp W,             &kp E,             &kp R,             &kp T,             &kp Y,             &kp U,             &kp I,             &kp O,             &kp P,             \
+////U_MT(LGUI, A),     U_MT(LALT, S),     U_MT(LCTRL, D),    U_MT(LSHFT, F),    &kp G,             &kp H,             U_MT(LSHFT, J),    U_MT(LCTRL, K),    U_MT(LALT, L),     U_MT(LGUI, SQT),   \
+////&kp Z,             &kp  X,            &kp C,             &kp V,             &kp B,             &kp N,             &kp M,             &kp COMMA,         &kp  DOT,          &kp  SLASH,\
+////U_NP,              U_NP,              U_LT(U_MEDIA, TAB),U_LT(U_NAV, ESC),U_LT(U_MOUSE, BSPC),U_LT(U_SYM, SPACE),  U_LT(U_NUM, RET), U_LT(U_FUN, DEL),  U_NP,              U_NP
+
+// LAYER from qmk version
+// #define MIRYOKU_LAYER_NUM \
+// KC_LBRC, KC_RBRC, KC_7, KC_8, KC_9, U_NA, TD(U_TD_U_BASE), TD(U_TD_U_EXTRA),
+// TD(U_TD_U_TAP), TD(U_TD_BOOT), \
+// KC_SCLN, KC_EQL, KC_4, KC_5, KC_6, U_NA, KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI,
+// \ KC_GRV, KC_BSLS, KC_1, KC_2, KC_3, U_NA, TD(U_TD_U_NUM), TD(U_TD_U_NAV),
+// KC_ALGR, U_NA, U_NP, U_NP, KC_DOT, KC_0, KC_MINS, U_NA, U_NA, U_NA, U_NP,
+// U_NP
+
+////#define MIRYOKU_LAYER_NUM \
+////&kp LBKT,          &kp RBKT,          &kp N7,            &kp N8,            &kp N9,            U_NA,              &u_to_U_BASE,      &u_to_U_EXTRA,     &u_to_U_TAP,       U_BOOT,            \
+////&kp SEMI,          &kp EQUAL,         &kp N4,            &kp N5,            &kp N6,            U_NA,              &kp LSHFT,         &kp LCTRL,         &kp LALT,          &kp LGUI,          \
+////&kp GRAVE,         &kp BSLH,          &kp N1,            &kp N2,            &kp N3,            U_NA,              &u_to_U_NUM,       &u_to_U_NAV,       &kp RALT,          U_NA,              \
+////U_NP,              U_NP,              &kp DOT,           &kp N0,            &kp MINUS,         U_NA,              U_NA,              U_NA,              U_NP,              U_NP
+
+// LAYER from qmk version
+// #define MIRYOKU_LAYER_SYM
+// KC_LCBR, KC_RCBR, KC_AMPR, KC_ASTR, KC_LPRN, U_NA, TD(U_TD_U_BASE),
+// TD(U_TD_U_EXTRA), TD(U_TD_U_TAP), TD(U_TD_BOOT), KC_COLN, KC_PLUS, KC_DLR,
+// KC_PERC, KC_CIRC, U_NA, KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI, KC_TILD, KC_PIPE,
+// KC_EXLM, KC_AT, KC_HASH, U_NA, TD(U_TD_U_SYM), TD(U_TD_U_MOUSE), KC_ALGR,
+// U_NA, U_NP, U_NP, KC_LPRN, KC_RPRN, KC_UNDS, U_NA, U_NA, U_NA, U_NP, U_NP
+
+////#define MIRYOKU_LAYER_SYM \
+////&kp LBRC,          &kp RBRC,          &kp AMPS,          &kp ASTRK,         &kp LPAR,          U_NA,              &u_to_U_BASE,      &u_to_U_EXTRA,     &u_to_U_TAP,       U_BOOT,            \
+////&kp COLON,         &kp PLUS,          &kp DLLR,          &kp PRCNT,         &kp CARET,         U_NA,              &kp LSHFT,         &kp LCTRL,         &kp LALT,          &kp LGUI,          \
+////&kp TILDE,         &kp PIPE,          &kp EXCL,          &kp AT,            &kp HASH,          U_NA,              &u_to_U_SYM,       &u_to_U_MOUSE,     &kp RALT,          U_NA,              \
+////U_NP,              U_NP,              &kp LPAR,          &kp RPAR,          &kp UNDER,         U_NA,              U_NA,              U_NA,              U_NP,              U_NP
